128a9e84f3ddd9e51f1c9460b4c9323ea7fc5417
Manuel Dinis <4228@feppv.pt>
2023-05-02 10:41:07 +0000

    - changed server

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const restaurants = require('./routes/restaurants');
 app.use(express.json());
 app.use("/", restaurants);
 
-const server = app.listen(3000, () => {
+const server = app.listen(3000, '10.0.2.11', () => {
   console.log("Server started on port 3000");
 });
