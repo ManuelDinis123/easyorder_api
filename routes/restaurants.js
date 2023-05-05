8652ec2a28a3b07064809bbf260895323d8760aa
Manuel Dinis <4228@feppv.pt>
2023-05-05 10:17:13 +0000

    - Gave a big value for max concat value

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -5,50 +5,52 @@ var db = require("../database.js");
 // Get all public restaurants and menu items
 app.get("/", async function (req, res) {
   const sql_where = restaurantFilters(req.body.filters);
-  db.query(
-    "SELECT a.id AS restaurant_id, a.name AS restaurant_name, a.logo_url, a.logo_name,\
-    CONCAT(\
-        '[',\
-        GROUP_CONCAT(\
-            CONCAT(\
-                '{\"name\":\"', c.name, '\",\"price\":', c.price, ',\"cost\":', c.cost, ',\"imageUrl\":\"', c.imageUrl, '\",\"side_items\": [',\
-                IFNULL(\
-                    (\
-                        SELECT GROUP_CONCAT(CONCAT('{\"name\":\"', d.ingredient, '\",\"price\":', d.price, '}') SEPARATOR ',')\
-                        FROM menu_item_ingredients d\
-                        WHERE d.menu_item_id = c.id\
-                    ),\
-                    ''\
-                ),\
-                ']}'\
-            )\
-            SEPARATOR ','\
-        ),\
-        ']'\
-    ) AS items \
-FROM restaurants a \
-INNER JOIN menu b ON a.id = b.restaurant_id \
-INNER JOIN menu_item c ON c.menu_id = b.id \
-WHERE a.isPublic = 1 " +
+  db.query("SET SESSION group_concat_max_len = 1000000;", function (err, rows) {
+    db.query(
+      "SELECT a.id AS restaurant_id, a.name AS restaurant_name, a.logo_url, a.logo_name,\
+      CONCAT(\
+          '[',\
+          GROUP_CONCAT(\
+              CONCAT(\
+                  '{\"name\":\"', c.name, '\",\"price\":', c.price, ',\"cost\":', c.cost, ',\"imageUrl\":\"', c.imageUrl, '\",\"side_items\": [',\
+                  IFNULL(\
+                      (\
+                          SELECT GROUP_CONCAT(CONCAT('{\"name\":\"', d.ingredient, '\",\"price\":', d.price, '}') SEPARATOR ',')\
+                          FROM menu_item_ingredients d\
+                          WHERE d.menu_item_id = c.id\
+                      ),\
+                      ''\
+                  ),\
+                  ']}'\
+              )\
+              SEPARATOR ','\
+          ),\
+          ']'\
+      ) AS items \
+  FROM restaurants a \
+  INNER JOIN menu b ON a.id = b.restaurant_id \
+  INNER JOIN menu_item c ON c.menu_id = b.id \
+  WHERE a.isPublic = 1 AND a.active = 1 " +
       sql_where +
       " \
-GROUP BY a.id, b.id",
-    function (err, rows) {
-      if (err) {
-        console.log(err);
-        return res
-          .status(500)
-          .send("Ocorreu um erro a ir buscar os restaurantes");
+  GROUP BY a.id, b.id",
+      function (err, rows) {
+        if (err) {
+          console.log(err);
+          return res
+            .status(500)
+            .send("Ocorreu um erro a ir buscar os restaurantes");
+        }
+        var final = rows;
+        count = 0;
+        final.forEach((values) => {
+          final[count]["items"] = JSON.parse(values.items);
+          count++;
+        });
+        res.send(final);
       }
-      var final = rows;
-      count = 0;
-      final.forEach((values) => {
-        final[count]["items"] = JSON.parse(values.items);
-        count++;
-      });
-      res.send(final);
-    }
-  );
+    );
+  })
 });
 
 // Get all restaurants and their reviews
@@ -58,8 +60,9 @@ app.get("/reviews", async function (req, res) {
   if (req.body.filters) {
     star_avg = req.body.filters["star_avg"];
   }
-  db.query(
-    `select a.id as restaurant_id, a.name as restaurant_name, a.logo_url, a.logo_name, round(sum(b.stars)/count(b.review)) as star_avg,
+  db.query("SET SESSION group_concat_max_len = 1000000;", function (err, rows) {
+    db.query(
+      `select a.id as restaurant_id, a.name as restaurant_name, a.logo_url, a.logo_name, round(sum(b.stars)/count(b.review)) as star_avg,
     concat(
     '[', group_concat(
       concat(
@@ -74,22 +77,23 @@ app.get("/reviews", async function (req, res) {
       `
     group by a.id` +
       (star_avg ? ` HAVING star_avg=` + star_avg : ``),
-    function (err, rows) {
-      if (err) {
-        console.log(err);
-        return res
-          .status(500)
-          .send("Ocorreu um erro a ir buscar os restaurantes");
+      function (err, rows) {
+        if (err) {
+          console.log(err);
+          return res
+            .status(500)
+            .send("Ocorreu um erro a ir buscar os restaurantes");
+        }
+        var final = rows;
+        count = 0;
+        final.forEach((values) => {
+          final[count]["reviews"] = JSON.parse(values.reviews);
+          count++;
+        });
+        res.send(final);
       }
-      var final = rows;
-      count = 0;
-      final.forEach((values) => {
-        final[count]["reviews"] = JSON.parse(values.reviews);
-        count++;
-      });
-      res.send(final);
-    }
-  );
+    )
+  });
 });
 
 // Place an order
@@ -113,16 +117,16 @@ app.post("/order", async function (req, res) {
     db.query(
       `INSERT INTO orders(ordered_at, ordered_by, restaurant_id, progress, deadline, closed)
               VALUES("` +
-        datetime +
-        `", "` +
-        req.body.user +
-        `", "` +
-        item +
-        `", 0, "` +
-        req.body.deadline +
-        `", "` +
-        0 +
-        `")`,
+      datetime +
+      `", "` +
+      req.body.user +
+      `", "` +
+      item +
+      `", 0, "` +
+      req.body.deadline +
+      `", "` +
+      0 +
+      `")`,
       (err2, result) => {
         if (err2) {
           return res.status(500).send("Erro a criar os pedidos");
@@ -135,7 +139,7 @@ app.post("/order", async function (req, res) {
             `SELECT
              a.id, a.price, a.cost
              FROM menu_item a INNER JOIN menu b ON b.id = a.menu_id INNER JOIN restaurants c on c.id = b.restaurant_id WHERE a.id = ` +
-              i,
+            i,
             function (err, rows) {
               if (err) {
                 return res
@@ -147,16 +151,16 @@ app.post("/order", async function (req, res) {
               db.query(
                 `INSERT INTO order_items(order_id, menu_item_id, quantity, price, cost)
                  VALUES(` +
-                  result.insertId +
-                  `,` +
-                  menu_item_data[0].id +
-                  `, ` +
-                  itemsids[i]["quantity"] +
-                  `, ` +
-                  menu_item_data[0].price +
-                  `, ` +
-                  menu_item_data[0].cost +
-                  `)`,
+                result.insertId +
+                `,` +
+                menu_item_data[0].id +
+                `, ` +
+                itemsids[i]["quantity"] +
+                `, ` +
+                menu_item_data[0].price +
+                `, ` +
+                menu_item_data[0].cost +
+                `)`,
                 function (err3, result2) {
                   if (err3) {
                     console.log(err3);
@@ -170,7 +174,7 @@ app.post("/order", async function (req, res) {
                       // Get price of the side item
                       db.query(
                         `SELECT price FROM menu_item_ingredients WHERE id=` +
-                          side,
+                        side,
                         function (err4, result3) {
                           if (err4) {
                             console.log(err4);
@@ -184,14 +188,14 @@ app.post("/order", async function (req, res) {
                           db.query(
                             `INSERT INTO order_items_sides(order_item_id, side_id, quantity, price)
                           VALUES (` +
-                              result2.insertId +
-                              `,` +
-                              side +
-                              `,` +
-                              side_items[side] +
-                              `,` +
-                              result3[0]["price"] +
-                              `)`,
+                            result2.insertId +
+                            `,` +
+                            side +
+                            `,` +
+                            side_items[side] +
+                            `,` +
+                            result3[0]["price"] +
+                            `)`,
                             function (err5, res4) {
                               if (err5) {
                                 console.log(err5);
@@ -242,8 +246,8 @@ function restaurantFilters(filters) {
         (filter == "logo_from_url" || filter == "logo_from_file"
           ? " IS NOT NULL"
           : filter == "restaurant_name"
-          ? " LIKE '%" + filters[filter] + "%'"
-          : " = " + filters[filter]);
+            ? " LIKE '%" + filters[filter] + "%'"
+            : " = " + filters[filter]);
     }
   });
   return sql_where;
